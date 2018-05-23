365094337b7da062ac6b00219c7504ea326a8e75
Rita Sukhova <nadobnomars@gmail.com>
2018-05-23 19:17:20 +0300

    initial commit

---
+++ GraphBuilder/App Delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphBuilder
+//
+//  Created by Маргарита on 23.05.2018.
+//  Copyright © 2018 Маргарита. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GraphBuilder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GraphBuilder
+//
+//  Created by Маргарита on 23.05.2018.
+//  Copyright © 2018 Маргарита. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
