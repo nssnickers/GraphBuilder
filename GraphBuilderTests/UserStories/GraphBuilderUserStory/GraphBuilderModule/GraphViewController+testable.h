d9407c214f48d505fc1abb654bbd84bd2839e7bc
Rita Sukhova <nadobnomars@gmail.com>
2018-05-24 15:08:30 +0300

    [add] graphview tests

---
+++ GraphBuilderTests/UserStories/GraphBuilderUserStory/GraphBuilderModule/GraphViewController+testable.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController+testable.h
+//  GraphBuilder
+//
+//  Created by Маргарита on 24.05.2018.
+//  Copyright © 2018 Маргарита. All rights reserved.
+//
+
+@interface GraphViewController ()
+
+- (void)didTapBuildButton:(id)sender;
+
+@end
